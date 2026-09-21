8210ba8b0157eec4eee80efa655edcbc2f2fe6b3
manas-123-cell <manas.bais.batch2025@sitnagpur.siu.edu.in>
2026-09-21 18:58:12 +0530

    Create 1.1.8.c

---
+++ Lab Programs/1.1.8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+	int r1, c1, r2, c2;
+	scanf("%d %d", &r1, &c1);
+	int A[r1][c1];
+	for (int i = 0; i < r1; i++)
+		for (int j = 0; j < c1; j++)
+			scanf("%d", &A[i][j]);
+	scanf("%d %d", &r2, &c2);
+	if (c1 != r2) {
+		printf("Invalid input");
+		return 0;
+	}
+	int B[r2][c2], C[r1][c2];
+	for (int i = 0; i < r2; i++)
+		for (int j = 0; j < c2; j++)
+			scanf("%d", &B[i][j]);
+	for (int i = 0; i < r1; i++) {
+		for (int j = 0; j < c2; j++) {
+			C[i][j] = 0;
+			for (int k = 0; k < c1; k++)
+				C[i][j] += A[i][k] * B[k][j];
+		}
+	}
+	for (int i = 0; i < r1; i++) {
+		for (int j = 0; j < c2; j++)
+			printf("%d ", C[i][j]);
+		printf("\n");
+	}
+	return 0;
+}
